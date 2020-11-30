ea1e60d29e33a94e9f7ac0e27a760b7ef3e51486
Anthony Burke <pandom@users.noreply.github.com>
2020-11-30 16:37:36 +1100

    Initial Factory

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
 
+output "project_info_example" {
+  value       = module.project-factory.project_id
+  description = "The ID of the created project"
+}
+
+output "domain_example" {
+  value       = module.project-factory.domain
+  description = "The organization's domain"
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,56 @@
 
+locals {
+  credentials_file_path = var.credentials_path
+}
+
+/******************************************
+  Provider configuration
+ *****************************************/
+provider "google" {
+  credentials = file(local.credentials_file_path)
+  version     = "~> 3.30"
+}
+
+provider "google-beta" {
+  credentials = file(local.credentials_file_path)
+  version     = "~> 3.38"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+
+module "project-factory" {
+  source                  = "../../"
+  random_project_id       = true
+  name                    = "simple-sample-project"
+  org_id                  = var.organization_id
+  billing_account         = var.billing_account
+  credentials_path        = local.credentials_file_path
+  default_service_account = "deprivilege"
+
+  activate_api_identities = [{
+    api = "healthcare.googleapis.com"
+    roles = [
+      "roles/healthcare.serviceAgent",
+      "roles/bigquery.jobUser",
+    ]
+  }]
+}
+
+variable "organization_id" {
+  description = "The organization id for the associated services"
+}
+
+variable "billing_account" {
+  description = "The ID of the billing account to associate this project with"
+}
+
+variable "credentials_path" {
+  description = "Path to a service account credentials file with rights to run the Project Factory. If this file is absent Terraform will fall back to Application Default Credentials."
+  default     = ""
+}
