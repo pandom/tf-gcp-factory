c1e9353f9f21026a96cd57625dc148be9478a079
Anthony Burke <pandom@users.noreply.github.com>
2020-12-02 11:06:24 +1100

    Output changes

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 
 output "project_info_example" {
-  value       = module.project-factory.project_id
+  value       = module.network_example_simple_project.project_id
   description = "The ID of the created project"
 }
 
-output "domain_example" {
-  value       = module.project-factory.domain
-  description = "The organization's domain"
+output "network name" {
+  value       = module.network_example_simple_project.network_name
+  description = "The networks's name"
 }
