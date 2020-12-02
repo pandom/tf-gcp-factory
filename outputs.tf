bf16b7aa6f32ff8448d56cdf312c278ab8eab7b8
Anthony Burke <pandom@users.noreply.github.com>
2020-12-02 14:37:25 +1100

    Fixes

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 
 output "project_info_example" {
-  value       = module.network_example_simple_project.project_id
+  value       = module.vpc.project_id
   description = "The ID of the created project"
 }
 
 output "network_name" {
-  value       = module.network_example_simple_project.network_name
+  value       = module.vpc.network_name
   description = "The networks's name"
 }
